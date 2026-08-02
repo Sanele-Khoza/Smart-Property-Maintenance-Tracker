0e6a79cc4aee3858b998a0668237df660c33386c
Sanele-Khoza <sanelerondo012@gmail.com>
2026-08-02 16:37:30 +0200

    Gave the Manager the privillegde to delete properties

--- backend/src/modules/properties/properties.routes.js
+++ backend/src/modules/properties/properties.routes.js
@@ -13,7 +13,7 @@ router.get('/', authenticate, authorize(Roles.TENANT, Roles.PROPERTY_MANAGER, Ro
 router.get('/:id', authenticate, authorize(Roles.TENANT, Roles.PROPERTY_MANAGER, Roles.SERVICE_PROVIDER, Roles.SYSTEM_ADMIN), ctrl.getById);
 router.post('/', authenticate, authorize(Roles.SYSTEM_ADMIN, Roles.PROPERTY_MANAGER), validate(createPropertySchema), auditLog('PROPERTY_CREATED', req => ({ type: 'property', id: null })), ctrl.create);
 router.put('/:id', authenticate, authorize(Roles.SYSTEM_ADMIN, Roles.PROPERTY_MANAGER), validate(updatePropertySchema), auditLog('PROPERTY_UPDATED', req => ({ type: 'property', id: req.params.id })), ctrl.update);
-router.delete('/:id', authenticate, authorize(Roles.SYSTEM_ADMIN), auditLog('PROPERTY_DELETED', req => ({ type: 'property', id: req.params.id })), ctrl.remove);
+router.delete('/:id', authenticate, authorize(Roles.SYSTEM_ADMIN, Roles.PROPERTY_MANAGER), auditLog('PROPERTY_DELETED', req => ({ type: 'property', id: req.params.id })), ctrl.remove);
 router.get('/:id/units', authenticate, authorize(Roles.TENANT, Roles.PROPERTY_MANAGER, Roles.SERVICE_PROVIDER, Roles.SYSTEM_ADMIN), ctrl.getUnits);
 router.get('/:id/tickets', authenticate, authorize(Roles.TENANT, Roles.PROPERTY_MANAGER, Roles.SERVICE_PROVIDER, Roles.SYSTEM_ADMIN), ctrl.getTickets);
 
